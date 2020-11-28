f66bded06d4106b68ea7badfeecf5506a79f2316
aflatness <aflatness@gmail.com>
2020-11-28 10:13:25 -0700

    'Sends hello express'

--- myApp.js
+++ myApp.js
@@ -1,1 +1,45 @@
-console.log('Hello World')
+
+var express = require('express');
+var app = express();
+
+console.log('Hello world');
+
+app.get('/', function (req, res) {
+  res.send('Hello Express');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
